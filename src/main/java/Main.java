52b0a20bd913f48f7e1a699aa0ba82374c3af65c
Pranjay Srivastava <pranjaysri5@gmail.com>
2026-06-14 23:35:00 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
         Scanner sc = new Scanner(System.in);
-
+        File currentDirectory = new File(System.getProperty("user.dir"));
         while (true) {
             System.out.print("$ ");
 
@@ -14,14 +14,24 @@ public class Main {
                 break;
             }
             else if (input.equals("pwd")){
-                System.out.println(System.getProperty("user.dir"));
+                // System.out.println(System.getProperty("user.dir"));   //for pwd function
+                System.out.println(currentDirectory.getAbsolutePath());  //for cd function
+            }
+            else if (input.startsWith("cd ")) {
+                String path = input.substring(3);
+                File newDir = new File(path);
+                if (newDir.exists() && newDir.isDirectory()) {
+                    currentDirectory = newDir;
+                } else {
+                    System.out.println("cd: " + path + ": No such file or directory");
+                }
             }
             else if (input.startsWith("echo ")) {
                 System.out.println(input.substring(5));
             }
             else if (input.startsWith("type ")) {
                 String cmd = input.substring(5);
-                if (cmd.equals("echo") || cmd.equals("exit") || cmd.equals("type") || cmd.equals("pwd")) {
+                if (cmd.equals("echo") || cmd.equals("exit") || cmd.equals("type") || cmd.equals("pwd") || cmd.equals("cd")) {
                     System.out.println(cmd + " is a shell builtin");
                 } 
                 else{
@@ -49,6 +59,7 @@ public class Main {
                     File file = new File(dir, parts[0]);
                     if (file.exists() && file.canExecute()) {
                         ProcessBuilder pb = new ProcessBuilder(parts);
+                        pb.directory(currentDirectory);
                         Process process = pb.start();
                         process.getInputStream().transferTo(System.out);
                         process.waitFor();

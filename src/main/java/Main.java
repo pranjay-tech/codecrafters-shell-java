5fa33a2532208e6a784d74c08bcfce482fade687
Pranjay Srivastava <pranjaysri5@gmail.com>
2026-06-15 02:57:30 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,49 @@
 import java.io.File;
 import java.util.Scanner;
 
+
 public class Main {
+    private static String[] parseCommand(String input) {
+        java.util.ArrayList<String> args = new java.util.ArrayList<>();
+        StringBuilder current = new StringBuilder();
+        boolean inSingleQuote = false;
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (c == '\'') {
+                inSingleQuote = !inSingleQuote;
+            }
+            else if (Character.isWhitespace(c) && !inSingleQuote) {
+                if (current.length() > 0) {
+                    args.add(current.toString());
+                    current.setLength(0);
+                }
+            }
+            else {
+                current.append(c);
+            }
+        }
+        if (current.length() > 0) {
+            args.add(current.toString());
+        }
+        return args.toArray(new String[0]);
+    }
+
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
         Scanner sc = new Scanner(System.in);
+        
+
         File currentDirectory = new File(System.getProperty("user.dir"));
         while (true) {
             System.out.print("$ ");
 
             String input = sc.nextLine();
+
+            String[] parts = parseCommand(input);
+
+            if(parts.length == 0){
+                continue;
+            }
             if(input.equals("exit")){
                 break;
             }
@@ -36,8 +70,14 @@ public class Main {
                     System.out.println("cd: " + path + ": No such file or directory");
                 }
             }
-            else if (input.startsWith("echo ")) {
-                System.out.println(input.substring(5));
+            else if (parts[0].equals("echo")) {
+                for (int i = 1; i < parts.length; i++) {
+                    if (i > 1) {
+                        System.out.print(" ");
+                    }
+                    System.out.print(parts[i]);
+                }
+                System.out.println();
             }
             else if (input.startsWith("type ")) {
                 String cmd = input.substring(5);
@@ -62,7 +102,6 @@ public class Main {
                 }
             }
             else {
-                String[] parts = input.split(" ");
                 String path = System.getenv("PATH");
                 boolean found = false;
                 for (String dir : path.split(File.pathSeparator)) {

aa86066e3db605941431a8f2e6ada7bfda571f19
Pranjay Srivastava <pranjaysri5@gmail.com>
2026-06-14 17:38:56 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,10 @@ public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
         System.out.print("$ ");
+
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        System.out.println(input + ": command not found");
     }
 }

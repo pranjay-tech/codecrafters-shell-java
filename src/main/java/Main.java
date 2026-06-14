85e267c69e59041f7498c790db96ea073c162982
Pranjay Srivastava <pranjaysri5@gmail.com>
2026-06-14 17:56:35 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,7 +14,16 @@ public class Main {
             }
             else if (input.startsWith("echo ")) {
                 System.out.println(input.substring(5));
-            } else {
+            }
+            else if (input.startsWith("type ")) {
+                String cmd = input.substring(5);
+                if (cmd.equals("echo") || cmd.equals("exit") || cmd.equals("type")) {
+                    System.out.println(cmd + " is a shell builtin");
+                } else {
+                    System.out.println(cmd + ": not found");
+                }
+            }
+            else {
                 System.out.println(input + ": command not found");
             }
         }
